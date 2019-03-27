8950449e11031bb0315c493e9241b3b356caaf02
Tales Marinho Godois <tales.godois@gmail.com>
2019-03-27 18:15:09 -0300

    adding context and children

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    getChildContext() {
+        return {
+            coloredTheme: "green"
+        };
+    }
+
+    render() {
+        return <Header>Hi children</Header>;
+    }
 }
+
+class C extends React.Component {
+    render() {
+        console.log(this.context);
+        const { children } = this.props;
+        console.log(children);
+        return (
+            <div style={{ color: this.context.coloredTheme }}>
+                <p>{this.context.coloredTheme}</p>
+                <p>{children}</p>
+            </div>
+        );
+    }
+}
+C.contextTypes = {
+    coloredTheme: PropTypes.string
+};
+
+function Header(params) {
+    const { children } = params;
+    return (
+        <div>
+            <C>Hi children</C>
+            <h1>{children}</h1>
+        </div>
+    );
+}
+
+App.childContextTypes = {
+    coloredTheme: PropTypes.string
+};
 
 export default App;
